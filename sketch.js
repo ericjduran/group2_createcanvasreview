7bac8d29e802c5fbd6e0f3ceb2d4b882924257a7
ericjduran <ace_e303@e303-s11.lab.lagcc>
2017-02-23 11:37:45 -0500

    group2_createcanvasreview

---
+++ sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+    createCanvas(700, 700);
+    background(51);
+}
+
+function draw() {
+    fill('rgb(100%,0%,10%)');
+    triangle(100, 600, 300, 100, 600, 600);
+    
+    fill(51);
+    ellipse(300, 400, 200, 200);
+    
+    fill(255, 204, 0);
+    ellipse(400, 400, 200, 200);
+    
+    fill(color(0,0, 255));
+    rect(250, 450, 200,200);
+
+    
+}
+  
